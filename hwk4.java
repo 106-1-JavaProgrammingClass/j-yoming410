7b09952cba9415d1e644c425e212560547d9f854
yoming410 <106021153@live.asia.edu.tw>
2017-09-30 16:04:10 +0800

    j-yoming410

---
+++ hwk4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hwk4 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int cm = scn.nextInt();
+        int kg = scn.nextInt();
+        System.out.println(cm / 2.54);
+        System.out.println(kg / 0.454);
+    }
+}
